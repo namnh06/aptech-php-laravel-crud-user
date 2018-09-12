76d9f7dda1b87c48d578bdcf444aee146e39486f
[name] <namnh.website@gmail.com>
2018-09-12 21:09:13 +0700

    updated favicon

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,12 +3,12 @@
     <div class="col-12">
       <div class="d-flex justify-content-between align-items-center">
         <span class="text-uppercase text-muted">
-      aptech php 13
+      aptech php laravel crud users demo
     </span>
         <form action="{{route('users.create')}}" method="get">
           <button type="submit" class="btn btn-info text-uppercase font-weight-bold rounded-0">
         create an user
-      </button>
+          </button>
         </form>
       </div>
     </div>
