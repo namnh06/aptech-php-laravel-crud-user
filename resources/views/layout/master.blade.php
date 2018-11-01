c18be0619135267a3213560c37342875c19ff8c3
namnh06 <namnh.developer@gmail.com>
2018-11-01 11:00:17 +0700

    added adsense

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -2,6 +2,13 @@
 <html lang="en">
 
 <head>
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+              google_ad_client: "ca-pub-2040865810414258",
+              enable_page_level_ads: true
+         });
+  </script>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" href="{{asset('./favicon.ico')}}">
